d05d29a2f11e955df8447f4ae2947f45cf781a3f
Kadija Ntieche <kadija.ntieche@ces>
2026-02-20 12:01:45 -0500

    Fix SPFx styles and build errors

---
+++ src/webparts/nosLocaux/NosLocauxWebPart.module.scss.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+require("./NosLocauxWebPart.module.css");
+const styles = {
+  nosLocaux: 'nosLocaux_4c0ba3e9',
+  title: 'title_4c0ba3e9',
+  locationsList: 'locationsList_4c0ba3e9',
+  locationCard: 'locationCard_4c0ba3e9',
+  blue: 'blue_4c0ba3e9',
+  locationName: 'locationName_4c0ba3e9',
+  locationIcon: 'locationIcon_4c0ba3e9',
+  pink: 'pink_4c0ba3e9',
+  locationInfo: 'locationInfo_4c0ba3e9',
+  locationAddress: 'locationAddress_4c0ba3e9',
+  locationCity: 'locationCity_4c0ba3e9',
+  editControls: 'editControls_4c0ba3e9',
+  editButton: 'editButton_4c0ba3e9',
+  deleteButton: 'deleteButton_4c0ba3e9',
+  addButton: 'addButton_4c0ba3e9',
+  formContainer: 'formContainer_4c0ba3e9',
+  formTitle: 'formTitle_4c0ba3e9',
+  formField: 'formField_4c0ba3e9',
+  formActions: 'formActions_4c0ba3e9',
+  saveButton: 'saveButton_4c0ba3e9',
+  cancelButton: 'cancelButton_4c0ba3e9',
+  errorText: 'errorText_4c0ba3e9',
+  container: 'container_4c0ba3e9'
+};
+
+export default styles;
+/* tslint:enable */
